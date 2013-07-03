704ad2bf4ef01dfc92a5c543454dd18786914afe
Linea Computers Srl <info@station.it>
2013-07-03 12:15:42 +0200

    ver 7.5
    LAURA: aggiornati bug per installazione android e apple

--- scripts/custom.js
+++ scripts/custom.js
@@ -142,7 +142,6 @@ $(document).ready(function(){
 	var isiPod = 		navigator.userAgent.toLowerCase().indexOf("ipod");
 	var isiAndroid = 	navigator.userAgent.toLowerCase().indexOf("android");
 	
-	var isPhonegap = 	document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
 	
 	if(isiPhone > -1) 	 {		 $('.ipod-detected').hide();		 $('.ipad-detected').hide();		 $('.iphone-detected').show();		 $('.android-detected').hide();	 }
 	if(isiPad > -1)	 {		 	 $('.ipod-detected').hide();		 $('.ipad-detected').show();		 $('.iphone-detected').hide();		 $('.android-detected').hide();	 }
@@ -153,13 +152,6 @@ $(document).ready(function(){
 		$('.ipad-detected').hide();		 
 		$('.iphone-detected').hide();		
 	 	$('.android-detected').show();	 
-	  	if ( queryObj[ "android" ] != "noinstall" ) {
-			 if (!isPhonegap)
-			 {
-			 	window.location = "android_install.html";
-			 }
-			
-	  	}
 		
 	 }  
 	//window.location = "android_install.html";
@@ -283,25 +275,6 @@ $(document).ready(function(){
 
 	
 });
-// ***this goes on the global scope
-	// get querystring as an array split on "&"
-	var querystring = location.search.replace( '?', '' ).split( '&' );
-	// declare object
-	
-	
-	// ***now you can use queryObj["<name>"] to get the value of a url
-	// ***variable
-
-		var queryObj = {};
-	// loop through each name-value pair and populate object
-	for ( var i=0; i<querystring.length; i++ ) {
-		  // get name and value
-		  var name = querystring[i].split('=')[0];
-		  var value = querystring[i].split('=')[1];
-		  // populate object
-		  queryObj[name] = value;
-	}
-		
 		
 	//$('.page-content').draggable({
 	//	axis:'x',
---
+++ scripts/detect.js
@@ -0,0 +1,59 @@
+
+
+$(document).ready(function(){
+
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	//Detect user agent for known mobile devices and show hide elements for each specific element
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	
+	var isiPhone = 		navigator.userAgent.toLowerCase().indexOf("iphone");
+	var isiPad = 		navigator.userAgent.toLowerCase().indexOf("ipad");
+	var isiPod = 		navigator.userAgent.toLowerCase().indexOf("ipod");
+	var isiAndroid = 	navigator.userAgent.toLowerCase().indexOf("android");
+	
+	var isPhonegap = 	document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+	
+	if(isiPhone > -1) 	 {		 $('.ipod-detected').hide();		 $('.ipad-detected').hide();		 $('.iphone-detected').show();		 $('.android-detected').hide();	 }
+	if(isiPad > -1)	 {		 	 $('.ipod-detected').hide();		 $('.ipad-detected').show();		 $('.iphone-detected').hide();		 $('.android-detected').hide();	 }
+	if(isiPod > -1)	 {		 	 $('.ipod-detected').show();		 $('.ipad-detected').hide();		 $('.iphone-detected').hide();		 $('.android-detected').hide();	 }   
+	if(isiAndroid > -1) 
+	{		 
+		$('.ipod-detected').hide();		 
+		$('.ipad-detected').hide();		 
+		$('.iphone-detected').hide();		
+	 	$('.android-detected').show();	 
+	  	if ( queryObj[ "android" ] != "noinstall" ) {
+			 if (!isPhonegap)
+			 {
+			 	window.location = "android_install.html";
+			 }
+			
+	  	}
+		
+	 }  
+	
+	
+
+
+	
+});
+// ***this goes on the global scope
+	// get querystring as an array split on "&"
+	var querystring = location.search.replace( '?', '' ).split( '&' );
+	// declare object
+	
+	
+	// ***now you can use queryObj["<name>"] to get the value of a url
+	// ***variable
+
+		var queryObj = {};
+	// loop through each name-value pair and populate object
+	for ( var i=0; i<querystring.length; i++ ) {
+		  // get name and value
+		  var name = querystring[i].split('=')[0];
+		  var value = querystring[i].split('=')[1];
+		  // populate object
+		  queryObj[name] = value;
+	}
+		
+		
